fd165c2c90cd4c58544d6f22c3e6a3a6fb4fa5a5
shinbilee <dltlsql8389@naver.com>
2025-02-06 13:44:40 +0900

    Refac: database async

--- app/database.py
+++ app/database.py
@@ -3,16 +3,31 @@
 # from sqlalchemy.orm import sessionmaker
 #
 # from app.core.config import settings  # 환경 변수 로드
-#
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.core.config import settings
+
 # # PostgreSQL 데이터베이스 연결 URL
-# SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 #
 # # 데이터베이스 엔진 생성 (PostgreSQL 설정)
 # engine = create_engine(
 #     SQLALCHEMY_DATABASE_URL,
 #     pool_pre_ping=True,  # 연결이 끊어진 세션을 감지하고 복구
 # )
-#
+# 비동기 SQLAlchemy 엔진 사용
+engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True, pool_pre_ping=True)
+
+# 세션을 비동기로 사용하도록 설정
+AsyncSession = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+    autocommit=False,
+    autoflush=False,
+)
+
 # # 세션 로컬 생성
 # SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 #
